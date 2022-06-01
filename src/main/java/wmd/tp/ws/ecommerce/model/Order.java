afbd9834f1ac7a0a4a7779bf1c1ce08a23282bf4
meryem-namiq <75445692+meryem-namiq@users.noreply.github.com>
2022-06-01 12:10:43 +0100

    Meryem MN

--- src/main/java/wmd/tp/ws/ecommerce/model/Order.java
+++ src/main/java/wmd/tp/ws/ecommerce/model/Order.java
@@ -1,5 +1,61 @@
 package wmd.tp.ws.ecommerce.model;
 
+import java.util.Date;
+
+@XmlRootElement
+@JsonbPropertyOrder({"id","customer","orderDate","items"})
 public class Order {
+	
+	private long id;
+	private Customer customer;
+	private Date orderDate;
+	
+	
+	public Order() {
+		super();
+	}
+
+
+	public Order(long id, Customer customer, Date orderDate) {
+		super();
+		this.id = id;
+		this.customer = customer;
+		this.orderDate = orderDate;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	
+	
+	
+	
 
 }
+
