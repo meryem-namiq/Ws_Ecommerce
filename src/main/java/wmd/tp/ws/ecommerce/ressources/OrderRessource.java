e79042f23a9dd4e8f3883da3ea47a6ebdfc4ef97
meryem-namiq <75445692+meryem-namiq@users.noreply.github.com>
2022-06-01 12:03:08 +0100

    Update OrderRessource.java

--- src/main/java/wmd/tp/ws/ecommerce/ressources/OrderRessource.java
+++ src/main/java/wmd/tp/ws/ecommerce/ressources/OrderRessource.java
@@ -1,5 +1,45 @@
 package wmd.tp.ws.ecommerce.ressources;
 
+import java.util.List;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import wmd.tp.ws.ecommerce.service.*;
+import wmd.tp.ws.ecommerce.model.*;
+
+@Path("/orders")
 public class OrderRessource {
+	
+	OrderManagementService orderManagementService = new OrderManagementService();
+	
+	@Get
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Order> getOrders(){
+		return orderManagementService.getOrders();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Order submitOrder(Order order) {
+		return orderManagementService.submitOrder(order);
+	}
+	
+	@PUT
+	@Path("/{orderId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Order updateOrder(@PathParam("orderId") long id , Order order) {
+		order.setId(id);
+		return orderManagementService.updateOrder(order);
+	}
+	
+	@GET
+	@Path("/{orderId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Order getOrderDetails(@PathParam("orderId") long id) {
+		return orderManagementService.getOrderDetails(id);
+	}
 
 }
