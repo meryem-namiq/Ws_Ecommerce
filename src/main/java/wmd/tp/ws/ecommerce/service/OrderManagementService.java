9cf0fe7686bbe29bea6e84e82b8efdb753bb4603
meryem-namiq <75445692+meryem-namiq@users.noreply.github.com>
2022-06-01 12:19:06 +0100

    Meryem

--- src/main/java/wmd/tp/ws/ecommerce/service/OrderManagementService.java
+++ src/main/java/wmd/tp/ws/ecommerce/service/OrderManagementService.java
@@ -1,5 +1,32 @@
 package wmd.tp.ws.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import wmd.tp.ws.ecommerce.model.Order;
+import wmd.tp.ws.ecommerce.repository.Repository;
+
 public class OrderManagementService {
+	
+	private Map<Long , Order> orders = Repository.getOrders();
+	
+	public List<Order>  getOrders(){
+		return new ArrayList<Order>(orders.values());
+		
+	}
+	
+	public Order getOrderDetails(long orderId) {
+		return orders.get(orderId);
+	}
+	
+	public Order updateOrder(Order order) {
+		orders.put(order.getId(), order);
+		return order;
+	}
+	
+	public Order cancelOrder(long orderId) {
+		return orders.remove(orderId);
+	}
 
 }
